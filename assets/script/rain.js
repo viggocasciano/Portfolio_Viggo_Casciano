6d4b2f50896d665f277c7ae0c9fe9eb597babee5
viggocasciano <115143549+viggocasciano@users.noreply.github.com>
2023-03-27 13:17:15 +0200

    Add files via upload

---
+++ assets/script/rain.js
@@ -0,0 +1,20 @@
+function rain(){
+    let amount = 100;
+    let body = document.getElementById('background_2');
+    let i =0;
+    while (i< amount){
+        let drop = document.createElement('i');
+
+        let size = Math.random() * 5;
+        let posX = Math.floor(Math.random() * window.innerWidth);
+        let delay = Math.random() * -20;
+        let duration = Math.random() * 5;
+        drop.style.width = 0.2 + size + "px";
+        drop.style.left = posX + "px";
+        drop.style.animationDelay = delay + "s";
+        drop.style.animationDuration = 3 + duration + "s";
+        body.appendChild(drop);
+        i++;
+    }
+}
+rain(); 
